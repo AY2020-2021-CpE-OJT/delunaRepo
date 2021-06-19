82b43942da9704c213f9a3872dcec8bba5b2c21d
rjmdeluna <rjmdeluna@gmail.com>
2021-06-19 11:33:10 +0800

    read to scan

--- src/delunaRepo/PrintAnInteger.java
+++ src/delunaRepo/PrintAnInteger.java
@@ -5,10 +5,10 @@ public class PrintAnInteger {
 	
 	    public static void main(String[] args) {
 
-	        Scanner reader = new Scanner(System.in);
+	        Scanner scan = new Scanner(System.in);
 	        System.out.print("Enter a digit: ");
 
-	        int number = reader.nextInt();
+	        int number = scan.nextInt();
 
 	        System.out.println("You entered: " + number);
 	}
